147011a0784e0feb5fb34fe144c9a6f393531c63
Erdem Toraman <erdem.toraman@futurice.com>
2018-11-30 13:16:58 +0200

    modified the test

--- loadbalancer/loadbalancer_test.go
+++ loadbalancer/loadbalancer_test.go
@@ -2,6 +2,8 @@ package loadbalancer
 
 import (
 	"log"
+	"math"
+	"runtime"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -11,19 +13,19 @@ import (
 func TestMaster_Submit(t *testing.T) {
 
 	wg := sync.WaitGroup{}
-	wg.Add(20000)
+	wg.Add(10000)
 
 	balancer := New(func(data interface{}) {
-		time.Sleep(time.Millisecond * time.Duration(data.(int)))
+		time.Sleep(time.Millisecond * time.Duration(math.Abs(float64(data.(int)-5000))))
 		wg.Done()
 	})
 
 	go func() {
 		for range time.Tick(time.Millisecond * 250) {
-			log.Printf("(worker, wip, wiq) (%d,%d,%d) \n", atomic.LoadInt32(&balancer.workerCount), atomic.LoadInt32(&balancer.wip), atomic.LoadInt32(&balancer.wiq))
+			log.Printf("(worker, wip, wiq) (%d,%d,%d) - GO: %d\n", atomic.LoadInt32(&balancer.workerCount), atomic.LoadInt32(&balancer.wip), atomic.LoadInt32(&balancer.wiq), runtime.NumGoroutine())
 		}
 	}()
-	for i := 0; i < 3000; i++ {
+	for i := 0; i < 10000; i++ {
 		balancer.Submit(i)
 		if i%100 == 0 {
 			time.Sleep(time.Millisecond * 500)
